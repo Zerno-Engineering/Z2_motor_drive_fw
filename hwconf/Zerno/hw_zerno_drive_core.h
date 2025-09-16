15d3dda41edb38969ec55766e59436746d126a08
Ayarde Christian <christianayarde@gmail.com>
2025-09-16 10:13:49 -0300

    Change dead time to 250ns

--- hwconf/Zerno/hw_zerno_drive_core.h
+++ hwconf/Zerno/hw_zerno_drive_core.h
@@ -330,7 +330,7 @@
 #define READ_HALL2()			palReadPad(HW_HALL_ENC_GPIO2, HW_HALL_ENC_PIN2)
 #define READ_HALL3()			palReadPad(HW_HALL_ENC_GPIO3, HW_HALL_ENC_PIN3)
 
-#define HW_DEAD_TIME_NSEC		50.0
+#define HW_DEAD_TIME_NSEC		250.0
 
 // Default setting overrides
 #ifndef MCCONF_L_MIN_VOLTAGE
