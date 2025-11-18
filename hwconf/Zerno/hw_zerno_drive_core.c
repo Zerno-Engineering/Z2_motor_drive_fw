7eb2ead66e05e8c05628da3b8b4b37d1b4130afa
Ayarde Christian <cayarde@paltatech.com>
2025-11-18 16:21:13 -0300

    zerno: enable encoder calibration
    Signed-off-by: Ayarde Christian <cayarde@paltatech.com>

--- hwconf/Zerno/hw_zerno_drive_core.c
+++ hwconf/Zerno/hw_zerno_drive_core.c
@@ -712,7 +712,7 @@ static THD_FUNCTION( speed_thread, arg )
                         is_default_erpm = true;
                         set_erpm_ramp_response();
                         encoder_calibrate_offset();
-                        //encoder_cal_detection();
+                        encoder_cal_detection();
                         store_minimum_value = true;
                     }
                 }
