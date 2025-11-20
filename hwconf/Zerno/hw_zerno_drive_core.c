5cbdb26c588cf496845b41e0d323d965ae46481f
Ayarde Christian <cayarde@paltatech.com>
2025-11-20 11:59:39 -0300

    zerno: take knob adc samples using a circular buffer
    Signed-off-by: Ayarde Christian <cayarde@paltatech.com>

--- hwconf/Zerno/hw_zerno_drive_core.c
+++ hwconf/Zerno/hw_zerno_drive_core.c
@@ -753,19 +753,37 @@ static THD_FUNCTION( encoder_thread, arg )
     eeprom_var step_value_stored;
 
     const float encoder_max_calibrated_value = MAX_ENCODER_VALUE_IN_VOLTS;
+    float sample_volts[ SAMPLES ];
+    float diff;
+    float get_encoder_sample_in_volts;
+    float circular_buffer_in_volts[ SAMPLES ];
+    uint8_t circular_index = 0;
+    uint8_t circular_counter = 0;
 
     for( ; ; )
     {
-        float sample_volts[ SAMPLES ];
-        float diff;
-        float get_encoder_sample_in_volts;
-
         if( !is_momentary_position_status )
         {
-            for( uint8_t i = 0; i < SAMPLES; i++ )
+            circular_buffer_in_volts[ circular_index ] = knob_read_in_volts;
+            circular_index = ( circular_index + 1 ) % SAMPLES;
+
+            if( circular_counter < SAMPLES )
             {
-                sample_volts[ i ] = knob_read_in_volts;
-                chThdSleepMilliseconds( 10 );
+                circular_counter++;
+            }
+
+            for( uint8_t i = 0; i < circular_counter; i++ )
+            {
+                sample_volts[ i ] = circular_buffer_in_volts[ ( circular_index + i ) % SAMPLES ];
+            }
+
+            // If buffer not yet full, fill remaining entries with the newest valid sample to keep downstream logic stable
+            if( circular_counter < SAMPLES )
+            {
+                for( uint8_t i = circular_counter; i < SAMPLES; i++ )
+                {
+                    sample_volts[ i ] = sample_volts[ circular_counter - 1 ];
+                }
             }
 
             for( uint8_t i = 0; i < SAMPLES; i++ )
